c7bac02a96eee3ae1908e4989d7c04a15b6c62dd
MitchellClarke <mtc5197@psu.edu>
2018-01-02 23:03:35 -0500

    wip

--- thermostat/main.py
+++ thermostat/main.py
@@ -1,15 +1,16 @@
 from Thermostat import Thermostat
 from Thermometer import Thermometer
 from Timer import Timer
+import RPi.GPIO as GPIO
 import time
 from Triggerable import Triggerable
 from threading import Thread
 
-TOP_TIMER_PIN = 40
-BOTTOM_TIMER_PIN = 38
-TOP_RADIANT_PIN = 36
-BOTTOM_RADIANT_PIN = 32
-TOP_THERMOMETER_PIN = 22
+TOP_TIMER_PIN = 29
+BOTTOM_TIMER_PIN = 28
+TOP_RADIANT_PIN = 27
+BOTTOM_RADIANT_PIN = 4
+TOP_THERMOMETER_PIN = 5
 
 if __name__ == "__main__":
     topTimerTrigger = Triggerable(TOP_TIMER_PIN)
@@ -17,11 +18,16 @@ if __name__ == "__main__":
     topRadiantTrigger = Triggerable(TOP_RADIANT_PIN)
     bottomRadiantTrigger = Triggerable(BOTTOM_RADIANT_PIN)
     
-    while True:
-        topTimerTrigger.on()
-        time.sleep(3)
-        topTimerTrigger.off()
-        time.sleep(3)
+	try:
+        while True:
+            topTimerTrigger.on()
+            time.sleep(3)
+            topTimerTrigger.off()
+            time.sleep(3)
+	except KeyboardInterrupt:
+	    print 'you killed me! :('
+	finally:
+		GPIO.cleanup()
     
     #topRadiantThermometer = Thermometer(TOP_THERMOMETER_PIN)
     #bottomRadiantThermometer = Thermometer(BOTTOM_THERMOMETER_PIN)
--- thermostat/triggerable.py
+++ thermostat/triggerable.py
@@ -4,7 +4,7 @@ import RPi.GPIO as GPIO
 class Triggerable:
 	def __init__(self, pin_num):
 		self.pin_num = pin_num
-		GPIO.setmode( GPIO.BOARD )
+		GPIO.setmode( GPIO.BCM )
 		GPIO.setup( self.pin_num, GPIO.OUT )
 
 	def on(self):
