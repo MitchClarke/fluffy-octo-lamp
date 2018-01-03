28224d5703cedffd06b3634ab1f21e900ebe1a97
MitchellClarke <mtc5197@psu.edu>
2018-01-02 20:27:33 -0500

    wip

--- thermostat/Thermostat.py
+++ thermostat/Thermostat.py
@@ -30,36 +30,3 @@ class Thermostat:
 			
 	def adjust_temp(self, new_temp):
 		self.target = new_temp
-	
-
-RELAY_PIN = 21
-DHT_PIN = 20
-
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup( RELAY_PIN, GPIO.OUT )
-GPIO.setup( DHT_PIN, GPIO.IN )
-
-sensor = DHT.DHT22
-setTemp = 85
-setHumidity = 60
-
-try:
-	while True:
-		humidity, temperature_C = DHT.read_retry( sensor, DHT_PIN )
-		temperature_F = temperature_C * 9/5.0 + 32
-		
-		print temperature_F, humidity
-		
-		if temperature_F < setTemp: 
-			GPIO.output( RELAY_PIN, GPIO.LOW )
-			print 'relay on'
-		else:
-			GPIO.output( RELAY_PIN, GPIO.HIGH )
-			print 'relay off'
-		time.sleep(5)
-except KeyboardInterrupt:
-	print 'You killed me :( '
-finally:
-	print 'cleaning up'
-	GPIO.cleanup()
