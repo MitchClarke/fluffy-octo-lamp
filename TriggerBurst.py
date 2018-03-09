ff79df0379e6de0af62497def5472566a6a78fd2
MitchellClarke <mtc5197@psu.edu>
2018-03-08 20:35:53 -0500

    wip

--- TriggerBurst.py
+++ TriggerBurst.py
@@ -4,7 +4,7 @@ import sys
 from time import sleep
 
 if __name__ == "__main__":
-    if length(sys.argv) != 4:
+    if len(sys.argv) != 4:
         print 'Expected 4 paramters'
         sys.exit(1)
     else:
